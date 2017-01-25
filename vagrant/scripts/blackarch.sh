60f5fe9ac475545a1b9b7d945dbf864418108226
Stefan Venz <stefan.venz@protonmail.com>
2017-01-25 08:47:48 +0100

    export gnupghome
    Vagrant Box creation ran into BlackArch/blackarch#1047 occasionally
    To avoid this export GNUPGHOME as mentioned in the issue
    
    Signed-off-by: Stefan Venz <stefan.venz@protonmail.com>

--- vagrant/scripts/blackarch.sh
+++ vagrant/scripts/blackarch.sh
@@ -5,6 +5,7 @@ set -e
 TERM=${TERM:-xterm}
 export TERM
 
+export GNUPGHOME=${HOME}/.gnupg/
 curl https://www.blackarch.org/strap.sh | bash
 NUMPKGS=$(/usr/bin/yaourt -Ss blackarch | /usr/bin/grep -v "^    " | /usr/bin/wc -l)
 echo "$NUMPKGS" BlackArch packages available.
