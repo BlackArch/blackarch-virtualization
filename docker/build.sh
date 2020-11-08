33cce7b6a83c8f25d58c2603de5b44ba340b3c47
thirdbyte <59290972+thirdbyte@users.noreply.github.com>
2020-11-09 02:17:41 +0530

    Docker: update build.sh (#22)
    * Added an additional expect condition for pacstrapping
    
    An additional -exact entry is required for a prompt that states "Do you want to skip the above package for this upgrade? [y/N]" to supply the answer with a "Y". The "... above package ..." here refers to the ones in $PKGIGNORE.
    
    * Added sed, pacman and procps-ng
    
    sed, pacman and procps-ng need to be installed during pacstrap as they are required by build-helper.sh script but do not get installed otherwise. Therefore, adding them to $PACMAN_EXTRA_PKGS.
    
    * Update build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -95,7 +95,7 @@ unset IFS
 
 PACMAN_CONF=(/usr/local/etc/build-pacman.conf)
 PACMAN_MIRRORLIST='Server = https://mirrors.kernel.org/archlinux/$repo/os/$arch'
-PACMAN_EXTRA_PKGS=''
+PACMAN_EXTRA_PKGS='sed pacman procps-ng base-devel'
 EXPECT_TIMEOUT=360
 ARCH_KEYRING=archlinux
 DOCKER_IMAGE_NAME=blackarch
@@ -117,6 +117,7 @@ expect <<EOF
 		-exact "anyway? \[Y/n\] " { send -- "n\r"; exp_continue }
 		-exact "(default=all): " { send -- "\r"; exp_continue }
 		-exact "installation? \[Y/n\]" { send -- "y\r"; exp_continue }
+		-exact "upgrade? \[y/N\]" { send -- "Y\r"; exp_continue }
 	}
 EOF
 
